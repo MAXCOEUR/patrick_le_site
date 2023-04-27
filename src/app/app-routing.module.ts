21460aec4a9750c481fa531d3e4c24d080b6ac05
COEUR MAXENCE p2102597 <maxence.coeur@etu.univ-lyon1.fr>
2023-04-27 12:27:30 +0200

    create all Component/Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JeuxComponent } from './jeux/jeux.component';
+import { PersonnageComponent } from './personnage/personnage.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  
+  {path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'jeux',component:JeuxComponent},
+  {path:'personnage',component:PersonnageComponent},
+
+  {path:'**',redirectTo:''}
 ];
 
 @NgModule({
---
+++ src/app/personnage/personnage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personnage',
+  templateUrl: './personnage.component.html',
+  styleUrls: ['./personnage.component.css']
+})
+export class PersonnageComponent {
+
+}
